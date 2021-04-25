dfdc9299ef5eada8fe0dc57622aa43bed28b5ff6
Robert Nguyen <robert@gotitapp.co>
2021-04-25 22:04:23 +0700

    fix: fixed typo for extra credit 2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -20,7 +20,7 @@ function createResource(promise) {
   )
   return {
     read() {
-      if (status = 'pending') throw result
+      if (status === 'pending') throw result
       if (status === 'error') throw result
       if (status === 'success') return result
       throw new Error('This should be impossible')
